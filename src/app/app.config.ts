400c76a868286dede486da712b4c96d3413379e3
cloudcoder88 <btech4104@gmail.com>
2025-09-24 18:43:54 +0100

    i deleted and reintalled the node _modules and i restarted the typescript server

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-
+import Aura from  '@primeuix/themes/aura';
 // Pick your theme preset
-import Aura from '@primeng/themes/aura';
 // or import Lara from '@primeng/themes/lara';
 // or import Nora from '@primeng/themes/nora';
 
