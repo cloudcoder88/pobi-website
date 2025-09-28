6d1e90e166ae007f1580f31e03cf128c95643f10
cloudcoder88 <btech4104@gmail.com>
2025-09-28 18:06:21 +0100

    i fixed prime ng rating issues,removed the binding style for order models

--- src/app/product-dialog/product-dialog.component.ts
+++ src/app/product-dialog/product-dialog.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';   
+import { RatingModule } from 'primeng/rating';
 
 @Component({
   selector: 'app-product-dialog',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatDialogModule, RatingModule,], 
   templateUrl: './product-dialog.component.html',
-  styleUrl: './product-dialog.component.css'
+  styleUrls: ['./product-dialog.component.css']
 })
 export class ProductDialogComponent {
-
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 }
