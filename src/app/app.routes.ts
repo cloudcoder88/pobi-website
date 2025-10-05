e698b185dcb1550cb11ba6cd4e5bf14cc6f1d919
cloudcoder88 <btech4104@gmail.com>
2025-10-05 16:34:09 +0100

    i added the checkout component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ReviewsComponent } from './reviews/reviews.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },          // Home
   { path: 'products', component: ProductsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'reviews', component: ReviewsComponent }, 
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'checkout', component: CheckoutComponent },
   { path: '**', redirectTo: '' } // fallback
 ];
