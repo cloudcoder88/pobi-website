85a3fdd4e6e7b30a0b765fccb4b400b06cf218c4
cloudcoder88 <btech4104@gmail.com>
2025-10-06 11:07:03 +0100

    i added the codes to the checkout components

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatButtonModule],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
+  cartItems: any[] = [];
+  totalPrice: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartItems = this.cartService.getCart();
+    this.totalPrice = this.cartService.getTotalPrice();
+  }
+
+  clearCart() {
+    this.cartService.clearCart();
+    this.cartItems = [];
+    this.totalPrice = 0;
+  }
+
+  checkoutWhatsApp() {
+    const message = this.cartService.generateWhatsAppMessage();
+    const phoneNumber = '2348147399481'; // put your phone number here
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  }
 }
