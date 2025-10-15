4f8980e7e3d7be92a6442f9a0c850dcb8ac40551
cloudcoder88 <btech4104@gmail.com>
2025-10-15 20:54:38 +0100

    i worked on the contact us page/half done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';//all the basic imports are imported here first
+
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contact={
+    fullName:'',
+    email:'',
+    message:'',
+    phone:'',
+  };
+
+  isLoading:boolean=false; //spinner flag
+  constructor(private http: HttpClient) {}
+  onSubmit(){
+    this.isLoading=true;
+    this.http.post('',this.contact).subscribe({
+      next:() =>{
+        alert('Message Sent Sucessfully !');
+        this.contact = {fullName:'',email:'',message:'',phone:'',}
+      }
+    })
+  }
+
 
 }
