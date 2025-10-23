359ad522ec999ac6228a04388671e5c185fff835
cloudcoder88 <btech4104@gmail.com>
2025-10-23 16:26:17 +0100

    i added support for the backend,wrote the component.ts and added validations and errors messages for the form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,10 @@ import { CommonModule } from '@angular/common';//all the basic imports are impor
 
 @Component({
   selector: 'app-contact',
-  imports: [FormsModule,CommonModule],
+  standalone:true,
+  imports: [FormsModule,CommonModule], //always import common modules for the *ngif and *ngfor directives
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
   contact={
@@ -19,23 +20,40 @@ export class ContactComponent {
     phone:'',
   };
 
-  isLoading:boolean=false; //spinner flag
+  isLoading:boolean=false; 
+  submitted =false; // add this to tract form submission status
+  isSucess:boolean =false; 
+serverMessage ='';// these properties are used to tract server responses
   constructor(private http: HttpClient) {}
-  onSubmit(){
-    this.isLoading=true;
+  onSubmit(contactForm:any){
+    this.submitted=true;  //accepts the form parameter and sets submitted to true
+
+    //check if form is valid before submitting it
+    if(contactForm.invalid){
+      alert('❌ Please fill out all the required fields correctly!.');
+      return;
+    }
+    this.isLoading =true;
+    this.serverMessage ='';
+    this.isSucess =false;
     this.http.post('',this.contact).subscribe({
-      next:(res) =>{
-        alert('Message Sent Sucessfully !');
+      next:(res) => {
+        //success -use the custom messages instead of the javascript alert  
+        this.serverMessage ='✅ Message sent successfully!';
+        this.isSucess =true;
         this.contact = {fullName:'',email:'',phone:'',message:''};
-        this.isLoading=false; //stops the spinner
+        this.isLoading = false;
+        this.submitted =false; //reset form submission status
+        contactForm.reset(); // this will clear the form validation states
       },
       error:(err) => {
-        console.error('Error Sending Messages:', err);
-        alert('Something Went Wrong.Please try again! .');
-        this.isLoading=false; //stops the spinner
+        //Error - use the custom message instead of the javascript alert
+        this.serverMessage = '❌ Something went wrong. Please try again.';
+        this.isSucess =false;
+        console.error('Error sending message:',err);
+        this.isLoading =false;
       }
+      
     });
   }
-
-
 }
